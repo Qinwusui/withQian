b0376b9e91f69b94699543e52bc8b217007e4cd2
Qinsansui <2064508450@qq.com>
2023-02-24 11:27:47 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,10 @@ group = "xyz.liusui"
 version = "1.0"
 
 repositories {
-//    maven("https://maven.aliyun.com/repository/central")
-//    maven("https://maven.aliyun.com/repository/public")
-//    maven("https://maven.aliyun.com/repository/google")
-//    maven("https://maven.aliyun.com/repository/gradle-plugin")
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.aliyun.com/repository/public")
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
     mavenLocal()
     mavenCentral()
 
